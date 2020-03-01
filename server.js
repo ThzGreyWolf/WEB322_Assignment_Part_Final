94a79101caa17deb726b8971e309f8a66792a685
Kirushanth Aruldas <kirushanth@live.ca>
2020-03-01 17:15:42 -0500

    + create acc verification

--- server.js
+++ server.js
@@ -67,9 +67,87 @@ app.post("/logIn", (req, res) => {
 });
 
 app.post("/createAcc", (req, res) => {
+    let nameNull = false;
+
+    let nameVal = "";
+    let emailVal = "";
+    let passVal = "";
+    let rePassVal = "";
+
+    let emailErrMess = "";
+    let passErrMess = "";
+    let rePassErrMess = "";
+
+    if(req.body.name == "") { nameNull = true; } else { nameVal = req.body.name; }
+
+    if(req.body.email == "") { 
+        emailErrMess = "Enter your Email";
+    } else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email)) {
+        emailErrMess = "Email not Valid";
+    } else {
+        emailVal = req.body.email;
+    }
+
+    let err = 0;
+    if(req.body.password == "") { 
+        passErrMess = "Enter a Password"; 
+    } else {
+        if(!/[a-z]/g.test(req.body.password)) {
+            passErrMess += "Password must have a lowercase charactor";
+            err = 1;
+        }
+
+        if(!/[A-Z]/g.test(req.body.password)) {
+            if(passErrMess == "") {
+                passErrMess += "Password must have an uppercase charactor";
+            } else {
+                passErrMess += ", an uppercase charactor";
+            }
+            err = 1;
+        }
+
+        if(!/[0-9]/g.test(req.body.password)) {
+            if(passErrMess == "") {
+                passErrMess += "Password must have a numeric value";
+            } else {
+                passErrMess += ", a numeric value";
+            }
+            err = 1;
+        }
+
+        if(!/[a-zA-Z0-9]{8,}/g.test(req.body.password)) {
+            if(passErrMess == "") {
+                passErrMess += "Password must be a 8 charactors or longer, without spaces";
+            } else {
+                passErrMess += ", and be 8 charactors or longer, without spaces";
+            }
+            err = 1;
+        }
+    }
+    if(err == 0) { 
+        passErrMess = "";
+        passVal = req.body.password;
+    }
+
+    if(req.body.passwordConf == "") { 
+        rePassErrMess = "Re enter your Password";
+    } else if (req.body.password != req.body.passwordConf){
+        rePassErrMess = "Passwords much match";
+    } else {
+        rePassVal = req.body.passwordConf;
+    }
+
     res.render("login", {
         title:"Create Account",
-        logInMode: false
+        logInMode: false,
+        nameVal: nameVal,
+        emailVal: emailVal,
+        passVal: passVal,
+        rePassVal: rePassVal,
+        nameErr: nameNull,
+        emailErr: emailErrMess,
+        passErr: passErrMess,
+        rePassErr: rePassErrMess
     });
 });
 
